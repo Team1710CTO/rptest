deac7885ee866e412b9c45c491f8353c73833be7
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2021-12-18 14:20:35 -0600

    mm

--- examples/lidar_test.py
+++ examples/lidar_test.py
@@ -24,7 +24,7 @@ try:
     for scan in lidar.iter_scans():
         for (_, angle, distance) in scan:
             scan_data[min([359, floor(angle)])] = distance
-        process_data(scan_data)
+        process_data(scan_data[0:10])
 
 except KeyboardInterrupt:
     print("Stopping.")
